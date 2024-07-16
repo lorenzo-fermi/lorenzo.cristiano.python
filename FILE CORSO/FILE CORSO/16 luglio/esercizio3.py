6c1b4cd5259f3f9c6879ef677180ee1109fb0020
lorenzo-fermi <lorenzo.cristiano.fermi@gmail.com>
2024-07-16 12:10:56 +0200

    Update esercizio3.py

--- FILE CORSO/FILE CORSO/16 luglio/esercizio3.py
+++ FILE CORSO/FILE CORSO/16 luglio/esercizio3.py
@@ -1,64 +1,43 @@
 import numpy as np
 
-# Crea un array di 50 numeri equidistanti tra 0 e 10
-array = np.linspace(0, 10, 50)
-
-# Crea un array di 50 numeri equidistanti tra 0 e 1
-array01 = np.linspace(0, 1, 50)
 
 while True:
-    # Stampa il menu delle opzioni
-    print("\nMenu:")
-    print("1. Stampare l'array 'array'")
-    print("2. Stampare l'array 'array01'")
-    print("3. Sommare 'array' e 'array01' e stampare il risultato")
-    print("4. Calcolare e stampare la somma di tutti gli elementi di 'array_sum'")
-    print("5. Calcolare e stampare la somma degli elementi di 'array_sum' maggiori di 5")
-    print("6. Esci")
+    print("menu")
+    print("1.creiamo l'array che ")
+    print("2. crea un array di 0 e 1")
+    print("3. somma i due array creati")
+    print("4. fai la somma degli elementi della somma dei due array")
+    print("5. somma solo i numeri del nuovo array maggiori di 5")
+    print("6. esci dal programma")
+    scelta=input("scegli cosa fare: ")
 
-    # Richiede all'utente di scegliere un'opzione
-    scelta = input("Scegli un'opzione: ")
-
-    if scelta == '1':
-        # Opzione 1: Stampare l'array 'array'
-        print("\nArray 'array':")
+    if scelta=="1":
+        array=np.linspace(0,10,50)
         print(array)
 
-    elif scelta == '2':
-        # Opzione 2: Stampare l'array 'array01'
-        print("\nArray 'array01':")
-        print(array01)
+    elif scelta=="2":
+        array01=np.linspace(0,1,50)
+        print(array)
 
-    elif scelta == '3':
-        # Opzione 3: Sommare 'array' e 'array01' e stampare il risultato
-        array_sum = array + array01
-        print("\nRisultato della somma di 'array' e 'array01':")
+    elif scelta=="3":
+        array_sum=array+array01
         print(array_sum)
+    
+    elif scelta=="4":
+        array_sum_sum=np.sum(array_sum)
+        print(array_sum_sum)
 
-    elif scelta == '4':
-        # Opzione 4: Calcolare e stampare la somma di tutti gli elementi di 'array_sum'
-        if 'array_sum' not in locals():
-            print("\nErrore: devi prima eseguire l'opzione 3 per calcolare 'array_sum'.")
-        else:
-            sum_array_sum = np.sum(array_sum)
-            print("\nSomma di tutti gli elementi di 'array_sum':", sum_array_sum)
+    elif scelta=="5":
+        array_sum_5 = np.sum(array_sum[array_sum > 5])
+        print(array_sum_5)
 
-    elif scelta == '5':
-        # Opzione 5: Calcolare e stampare la somma degli elementi di 'array_sum' maggiori di 5
-        if 'array_sum' not in locals():
-            print("\nErrore: devi prima eseguire l'opzione 3 per calcolare 'array_sum'.")
-        else:
-            sum_array_sum_5 = np.sum(array_sum[array_sum > 5])
-            print("\nSomma degli elementi di 'array_sum' maggiori di 5:", sum_array_sum_5)
-
-    elif scelta == '6':
-        # Opzione 6: Esci dal programma
-        print("\nUscita dal programma.")
+    elif scelta=="6":
+        print("uscita dal programma in corso")
         break
 
-    else:
-        # Opzione non valida
-        print("\nScelta non valida. Per favore, scegli un'opzione valida.")
+
+    
+
 
 
 
